d128d65c3a88ce0dfd8f935df4a6fa10e569f8e6
Junior Otero <dgrotero06@gmail.com>
2023-05-30 23:50:11 -0500

    Add ingress nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Testing success");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
