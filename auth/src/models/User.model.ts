b4c1bf8445a1c2e0fe9190327f95c903acb5cd4b
Junior Otero <dgrotero06@gmail.com>
2023-06-04 23:13:40 -0500

    add hooks to user moder to hash password to save

--- auth/src/models/User.model.ts
+++ auth/src/models/User.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../helpers/password";
 
 // Describe que se necesita para crear un usuarios
 interface UserAttrs {
@@ -28,6 +29,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
