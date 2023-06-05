b493772610825736bb45525c17cba50a15e0e8d2
Junior Otero <dgrotero06@gmail.com>
2023-06-04 23:12:17 -0500

    create bad request error

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -1,5 +1,11 @@
 import { RequestValidationError } from "./request-validation.error";
 import { DataBaseConnectionError } from "./database-connection.error";
+import { BadRequestError } from "./bad-request.error";
 import { NotFoundError } from "./not-found.error";
 
-export { RequestValidationError, DataBaseConnectionError, NotFoundError };
+export {
+  RequestValidationError,
+  DataBaseConnectionError,
+  NotFoundError,
+  BadRequestError,
+};
