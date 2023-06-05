837e96f92c8e5a8c77b007873373e001432bc6ef
Junior Otero <dgrotero06@gmail.com>
2023-06-04 20:40:21 -0500

    Connection to mongo db
    - create deployment and service to mongo db
    - connect the apllicatcion to database

--- auth/src/config/index.ts
+++ auth/src/config/index.ts
@@ -7,3 +7,4 @@ if (process.env.NODE_ENV !== "production") {
 export const PORT = process.env.PORT;
 export const SERVICE_NAME = process.env.SERVICE_NAME;
 export const CACHE_KEY = process.env.CACHE_KEY;
+export const MONGO_URI = process.env.MONGO_URI;
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { Routes } from "./routes";
 import { PORT, SERVICE_NAME } from "./config";
+import dbConnection from "./database/database-connection";
 
 const app = express();
 app.use(express.json());
 
 app.use(Routes);
 
-app.listen(PORT, () => {
-  console.log(SERVICE_NAME, ": Listening in port ", PORT);
-});
+const start = () => {
+  dbConnection();
+  app.listen(PORT, () => {
+    console.log(SERVICE_NAME, ": Listening in port ", PORT);
+  });
+};
+
+start();
