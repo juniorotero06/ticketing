19c19cbfd7c6f21d3c44784549ec21481582c9e8
Junior Otero <dgrotero06@gmail.com>
2023-06-04 21:17:30 -0500

    interfaces for the correct use between ts and mgs

--- auth/src/models/User.model.ts
+++ auth/src/models/User.model.ts
@@ -1,5 +1,24 @@
 import mongoose from "mongoose";
 
+// Describe que se necesita para crear un usuarios
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Describe como se ve toda la coleccion de usuarios, o los metodos asociados a la coleccion de usuarios
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+//Describe que propiedades tiene un solo usuario
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  //   createdAt: string;
+  //   updatedAt: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +30,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
